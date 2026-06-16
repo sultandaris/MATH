f4c2cfa1252d2a18233507f39a1170dc9ae87b33
sultandaris <kishouarima94@gmail.com>
2026-06-16 10:31:44 +0700

    Implement show method to display matrix contents

--- matrix.py
+++ matrix.py
@@ -2,7 +2,14 @@ class Matrix:
     def __init__(self,x):
         self.matrix = x
     
+    def show(self):
+        column = len(self.matrix)
+        for i in range(column):
+            print(self.matrix[i])
+    
 kotak = Matrix([
     [1,2],
     [3,4]
 ])
+
+kotak.show()
