0c3976890c5d0c755fcd51703bc230bd88e5729b
darissultan <sultan.daris.nuryanto-2022@fst.unair.ac.id>
2026-06-18 16:58:58 +0700

    add transpose function and wip in add function

--- matrix.py
+++ matrix.py
@@ -8,15 +8,29 @@ class Matrix:
         column = len(self.matrix)
         for i in range(column):
             print(self.matrix[i])
+        print()
 
     def transpose(self):
         result = []
-        for i in range(self.row):
-            d
-    
+        for i in range(self.column):
+            rows = []
+            for j in range(self.row):
+                rows.append(self.matrix[j][i])
+            result.append(rows)
+
+        print("---",self.row,self.column)
+        return Matrix(result)
+
+    def add(self, val):
+        if(self.row == val.row and self.column == self.row):
+            result
+       
 kotak = Matrix([
-    [1,2,3],
-    [4,5,6]
+    [2,3,3],
+    [5,6,9]
 ])
 
 kotak.show()
+kotakT = kotak.transpose()
+kotak.show()
+kotak.add(kotakT)
