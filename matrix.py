54451514f39acf0760dbac2e276b2ea49d2f14c7
darissultan <sultan.daris.nuryanto-2022@fst.unair.ac.id>
2026-06-18 16:31:03 +0700

    solving transpose problem

--- matrix.py
+++ matrix.py
@@ -1,15 +1,22 @@
 class Matrix:
     def __init__(self,x):
         self.matrix = x
+        self.column = len(self.matrix[0])
+        self.row = len(self.matrix)
     
     def show(self):
         column = len(self.matrix)
         for i in range(column):
             print(self.matrix[i])
+
+    def transpose(self):
+        result = []
+        for i in range(self.row):
+            d
     
 kotak = Matrix([
-    [1,2],
-    [3,4]
+    [1,2,3],
+    [4,5,6]
 ])
 
 kotak.show()
